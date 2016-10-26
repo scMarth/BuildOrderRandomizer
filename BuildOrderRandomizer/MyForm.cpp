466af7bfdeb86323beb1e4b4901f7fddc2b24ba1
scMarth <vlantaca@gmail.com>
2016-10-26 16:37:44 -0700

    Changed filenames

---
+++ BuildOrderRandomizer/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+
+void main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	BuildOrderRandomizer::MyForm form;
+	Application::Run(%form);
+
+}
+
--- README.md
+++
@@ -1,3 +0,0 @@
-This is a Work-in-Progress for a Starcraft Build Order Randomizer Application
-
-To use, copy the .exe file in Debug/ and copy the Matchup directories to a folder. Then add your build orders text files to each matchup folder and enjoy.
