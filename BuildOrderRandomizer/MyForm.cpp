e21ab7f8bc030e46bd8d480ea575161625372f83
scMarth <vlantaca@gmail.com>
2017-03-27 12:32:04 -0700

    added Windows application release builds

--- BuildOrderRandomizer/MyForm.cpp
+++ BuildOrderRandomizer/MyForm.cpp
@@ -1,10 +1,11 @@
+
 #include "MyForm.h"
 
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThread]
 
-void main()
+void WinMain()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
